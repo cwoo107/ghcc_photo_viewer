1df4f8dab857f734b807245847b0921a2b48c3ea
Caleb Woods <woodscaleb@gmail.com>
2019-09-22 18:43:21 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,119 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawerRight"
+      app
+      clipped
+      right
+      
+    >
+      <v-list dense>
+        <div v-for="album in list" :key="album">
+        <v-list-item @click.stop="right = !right" >
+          <v-list-item-action>
+          </v-list-item-action>
+          <v-list-item-content>
+            
+              <a :href="'/'+album.id">{{album.title}}</a>
+            
+          </v-list-item-content>
+        </v-list-item>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-right
+      color="blue-grey"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Toolbar {{id}}</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item @click.stop="left = !left">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Open Temporary Drawer</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      v-model="left"
+      fixed
+      temporary
+    ></v-navigation-drawer>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          justify="center"
+          align="center"
+        >
+        <div v-for="image in info" :key='image'>
+          <v-col class="shrink">
+            
+              <img :src="image.image.thumb.url" >
+            
+          </v-col>
+          </div>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-navigation-drawer
+      v-model="right"
+      fixed
+      right
+      temporary
+    ></v-navigation-drawer>
+
+    <v-footer
+      app
+      color="blue-grey"
+      class="white--text"
+    >
+      <span>Vuetify</span>
+      <div class="flex-grow-1"></div>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      id: null,
+      list: null,
+      info: null,
+      drawer: false,
+      drawerRight: false,
+      right: false,
+      left: false,
+    }),
+  mounted () {
+    axios
+      .get('https://ghcc-photos.herokuapp.com/albums.json').then(response => (this.list = response.data),axios .get('https://ghcc-photos.herokuapp.com/album_attachments.json?album_id='+this.$route.params.id+'')
+      .then(response => (this.info = response.data)))
+      
   }
-}
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+    // dynamic segments start with a colon
+    { path: '/:id', component: App }
+  ]
+var router = new VueRouter({
+    routes: routes,
+    mode: 'history'
+  })
 
 new Vue({
-  render: h => h(App),
+  router: router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
